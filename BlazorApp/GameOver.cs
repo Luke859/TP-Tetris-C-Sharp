0af5c987c921153d937e6c21f11cd387a82620fd
t.bourmaud <tao.bourmaud@ynov.com>
2021-12-06 15:52:35 +0100

    .

--- BlazorApp/GameOver.cs
+++ BlazorApp/GameOver.cs
@@ -1,23 +1,23 @@
-using System;
-using System.Collections;
+// using System;
+// using System.Collections;
 
-namespace BlazorApp {
+// namespace BlazorApp {
 
-    public class GameOver : Grid {
+//     public class GameOver : Grid {
 
-        public Grid[,] grid = new Grid[width, height];
+//         // public Grid[,] grid = new Grid[width, height];
         
-        public void GameIsOver()
-        {
-            for(int j = 0; j < width; j++)
-            {
-                if(grid[j, height - 1] != null)
-                {
-                    Console.WriteLine("GameOver !!");
-                    Environment.Exit(0);
-                }
-            }
+//         public void GameIsOver()
+//         {
+//             for(int j = 0; j < width; j++)
+//             {
+//                 if(grid[j, height - 1] != null)
+//                 {
+//                     Console.WriteLine("GameOver !!");
+//                     Environment.Exit(0);
+//                 }
+//             }
             
-        }
-    }
-}
+//         }
+//     }
+// }
--- BlazorApp/Grid.cs
+++ BlazorApp/Grid.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 
 
 
 namespace BlazorApp
 {
-    public class Grid : BlocksShape
+    public class Grid
     {
 
         public int width{get; set; }= 10;
